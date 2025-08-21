34dee457c11201de5c283be19141db6923ae0132
tanishs26 <tanishsharma2627@gmail.com>
2025-08-21 19:14:04 +0530

    fixed some bugs

--- src/app/liked/components/LikedContent.tsx
+++ src/app/liked/components/LikedContent.tsx
@@ -1,40 +1,37 @@
-"use client"
-import { useRouter } from 'next/navigation';
-import { Song } from '../../../../types';
-import { useUser } from '@/hooks/useUser';
-import { useEffect } from 'react';
-import LikedItem from './LikedItem';
-interface LikedContentProps{
-    songs:Song[]
+"use client";
+import { useRouter } from "next/navigation";
+import { Song } from "../../../../types";
+import { useUser } from "@/hooks/useUser";
+import { useEffect } from "react";
+import LikedItem from "./LikedItem";
+interface LikedContentProps {
+  songs: Song[];
 }
 
-const LikedContent:React.FC<LikedContentProps> = ({songs}) => {
-     const router=useRouter()
-     const {isLoading,user}=useUser()
-     useEffect(()=>{
-        if(isLoading){
-            router.replace('/')
-        }
-     },[isLoading,user,router])
-     if(songs.length===0){
-        return(
-            <div className='text-center mt-[50%] font-semibold text-neutral-400 text-3xl'>No Favourites.</div>
-        )
-     }else{
-        return(
-            <div>
-                {
-                    songs.map(song=>(
+const LikedContent: React.FC<LikedContentProps> = ({ songs }) => {
+  const router = useRouter();
+  const { isLoading, user } = useUser();
+  useEffect(() => {
+    if (isLoading) {
+      router.replace("/");
+    }
+  }, [isLoading, user, router]);
+  if (songs.length === 0) {
+    return (
+      <div className="text-center mt-[50%] font-semibold text-neutral-400 text-3xl">
+        No Favourites.
+      </div>
+    );
+  } else {
+    return (
+      <div className="w-full h-full overflow-y-auto">
+        {songs.map((song) => (
+          <LikedItem key={song.id} song={song} />
+        ))}
 
-                        <LikedItem key={song.id} song={song} />
-                    ))
-                }
-
-            </div>
-        )
-     }
-    
-   
-}
+      </div>
+    );
+  }
+};
 
 export default LikedContent;
--- src/app/liked/page.tsx
+++ src/app/liked/page.tsx
@@ -6,12 +6,11 @@ import LikedContent from "./components/LikedContent";
 export const revalidate = 0;
 const Liked = async () => {
   const songs = await getLikedSong();
-  console.log("liked songs:", songs);
 
   return (
     <div className="h-full w-full overflow-hidden overflow-y-auto">
       <header>
-        <div className="flex flex-col md:flex-row items-center gap-x-5 border-b-1 justify-center lg:justify-start gap-y-5  text-gray-800 pb-4 ">
+        <div className="flex flex-col md:flex-row items-center gap-x-5 border-b-1 justify-center lg:justify-start gap-y-5  pb-4 ">
           <div className="relative w-32 h-32 lg:h-44 lg:w-44">
             <Image fill src="/liked.png" alt="Nope" className="rounded-lg ml-2" />
           </div>
